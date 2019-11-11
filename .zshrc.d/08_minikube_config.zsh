b996f9b001072d7358f1e0af8afcc5964676f0f5
Sam <sam.rossoff@gremlin.com>
2019-11-11 10:23:08 -0800

    Disable this until I can figure out what broke

--- .zshrc.d/08_minikube_config.zsh
+++ .zshrc.d/08_minikube_config.zsh
@@ -1,10 +1,12 @@
-if [ "$(minikube status | grep -c Stopped)" -ge 1 ]; then
-  minikube start
-fi
 
-if [ "$(minikube status | grep -c Stopped)" -ge 1 ]; then
-  echo "Unable to start minikube, docker not setup"
-else
-  eval $(minikube docker-env)
-fi
+# Until I can get this sorted
+#if [ "$(minikube status | grep -c Stopped)" -ge 1 ]; then
+#  minikube start
+#fi
+#
+#if [ "$(minikube status | grep -c Stopped)" -ge 1 ]; then
+#  echo "Unable to start minikube, docker not setup"
+#else
+#  eval $(minikube docker-env)
+#fi
 
