af343fc08ff64dccf33e831cd5f73ead5dae6c8f
Sam <sam.rossoff@gremlin.com>
2020-08-20 11:38:55 -0700

    setup history

--- .zshrc.d/02_environment.zsh
+++ .zshrc.d/02_environment.zsh
@@ -2,3 +2,18 @@
 export LANG=en_US.UTF-8
 
 export EDITOR='vim'
+
+
+# Setup history
+export HISTFILE=~/.zsh_history
+export HISTFILESIZE=1000000000
+export HISTSIZE=1000000000
+
+setopt INC_APPEND_HISTORY
+export HISTTIMEFORMAT="[%F %T] "
+
+# Don't find dups (note, I still write them)
+setopt HIST_FIND_NO_DUPS
+
+# setup Github token
+export GITHUB_TOKEN=`cat ~/.config/gh/github.token`
