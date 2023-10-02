0b9e4e82c8cd1cf1a5f6935b9ba529c18b674591
Sam <sam.rossoff@gremlin.com>
2023-10-02 16:37:58 -0700

    Add Find-All-Electron-Versions script

---
+++ bin/find-all-electron-versions.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+# Taken from https://gist.github.com/april/3da7c3720b0d9f3ee7dc9a95f623578d
+
+# patched versions for CVE-2023-4863: 22.3.24, 24.8.3, 25.8.1, 26.2.1
+mdfind "kind:app" 2>/dev/null | sort -u | while read app;
+do
+  filename="$app/Contents/Frameworks/Electron Framework.framework/Electron Framework"
+  if [[ -f $filename ]]; then
+    echo "App Name:          $(basename ${app})"
+
+    electronVersion=$(strings "$filename" | grep "Chrome/" | grep -i Electron | grep -v '%s' | sort -u | cut -f 3 -d '/')
+    echo "Electron Version:  $electronVersion"
+
+    echo -n "File Name:         $filename "
+    echo -e "\n"
+  fi
+done
